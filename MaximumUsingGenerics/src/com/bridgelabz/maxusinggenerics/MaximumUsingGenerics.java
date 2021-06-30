5698ae776cd7e38eb528e1bd7cfdb43f196a21ee
SanketTambe39 <sankettambe39@gmail.com>
2021-07-01 00:41:02 +0530

    ADDED: Initial Files

---
+++ MaximumUsingGenerics/src/com/bridgelabz/maxusinggenerics/MaximumUsingGenerics.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.maxusinggenerics;
+
+public class MaximumUsingGenerics {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome To Maimum of three using Generics Program.");
+		
+	}
+
+}
