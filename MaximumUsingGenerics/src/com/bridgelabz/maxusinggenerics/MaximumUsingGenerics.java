5e86619abe302196e37217d99030277c04f45ef2
SanketTambe39 <sankettambe39@gmail.com>
2021-07-01 01:25:36 +0530

    MODIFIED: All three methods replaced with one generic method

--- MaximumUsingGenerics/src/com/bridgelabz/maxusinggenerics/MaximumUsingGenerics.java
+++ MaximumUsingGenerics/src/com/bridgelabz/maxusinggenerics/MaximumUsingGenerics.java
@@ -6,65 +6,27 @@ public class MaximumUsingGenerics {
 	static Float maxFloat;
 	static String maxString;
 	
-	public static int maximumInteger(Integer intFirst, Integer intSecond, Integer intThird)
-	{
-		maxInt = intFirst;
-		if(intSecond.compareTo(maxInt) > 0)
+	public static <E extends Comparable<E>> E maximum(E first,E second,E third) {
+		E max = first;
+		if(second.compareTo(max) > 0)
 		{
-			maxInt = intSecond;
+			max = second;
 		}
-		if(intThird.compareTo(maxInt) > 0)
+		if(third.compareTo(max) > 0)
 		{
-			maxInt = intThird;
+			max = third;
 		}
-		return maxInt;
+		return max;
 	}
-	
-	public static Float maximumFloat(Float floatFirst, Float floatSecond, Float floatThird)
-	{
-		maxFloat = floatFirst;
 		
-		if(floatSecond.compareTo(maxFloat) > 0)
-		{
-			maxFloat = floatSecond;
-		}
-		if(floatThird.compareTo(maxFloat) > 0)
-		{
-			maxFloat = floatThird;
-		}
-		return maxFloat;
-	}
-
-	public static String maximumString(String stringFirst, String stringSecond, String stringThird)
-	{
-		maxString = stringFirst;
-		
-		if(stringSecond.compareTo(maxString) > 0)
-		{
-			maxString = stringSecond;
-		}
-		if(stringThird.compareTo(maxString) > 0)
-		{
-			maxString = stringThird;
-		}
-		return maxString;
-	}
-	
 	public static void main(String[] args) {
 		
 		Integer intFirst = 23, intSecond = 52, intThird = 92;
 		Float floatFirst = 121.03f, floatSecond = 235.736f, floatThird = 119.009f;
 		String stringFirst = "Apple", stringSecond = "Peach", stringThird = "Banana";
 		
-		maximumInteger(intFirst, intSecond, intThird);
-		System.out.println("The Maximum Integer Number is  :: "+ maxInt);
-	
-		maximumFloat(floatFirst, floatSecond, floatThird);
-		System.out.println("The Maximum Float Number is  :: "+ maxFloat);
-		
-		maximumString(stringFirst, stringSecond, stringThird);
-		System.out.println("The Maximum String Number is  :: "+ maxString);
-		
+		System.out.println("The Maximum Integer Number is  :: "+ maximum(intFirst,intSecond,intThird));	
+		System.out.println("The Maximum Float Number is  :: "+ maximum(floatFirst, floatSecond, floatThird));
+		System.out.println("The Maximum String Number is  :: "+maximum(stringFirst, stringSecond,stringThird));
 	}
-
 }
