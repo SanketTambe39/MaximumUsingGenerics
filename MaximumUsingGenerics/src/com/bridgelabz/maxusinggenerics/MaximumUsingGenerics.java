52ad127e194c55643041fe8441734429acbbe38c
SanketTambe39 <sankettambe39@gmail.com>
2021-07-01 01:07:11 +0530

    ADDED: finding maximum size of a string

--- MaximumUsingGenerics/src/com/bridgelabz/maxusinggenerics/MaximumUsingGenerics.java
+++ MaximumUsingGenerics/src/com/bridgelabz/maxusinggenerics/MaximumUsingGenerics.java
@@ -4,6 +4,7 @@ public class MaximumUsingGenerics {
 	
 	static Integer maxInt;
 	static Float maxFloat;
+	static String maxString;
 	
 	public static int maximumInteger(Integer intFirst, Integer intSecond, Integer intThird)
 	{
@@ -34,16 +35,36 @@ public class MaximumUsingGenerics {
 		return maxFloat;
 	}
 
+	public static String maximumString(String stringFirst, String stringSecond, String stringThird)
+	{
+		maxString = stringFirst;
+		
+		if(stringSecond.compareTo(maxString) > 0)
+		{
+			maxString = stringSecond;
+		}
+		if(stringThird.compareTo(maxString) > 0)
+		{
+			maxString = stringThird;
+		}
+		return maxString;
+	}
+	
 	public static void main(String[] args) {
 		
 		Integer intFirst = 23, intSecond = 52, intThird = 92;
 		Float floatFirst = 121.03f, floatSecond = 235.736f, floatThird = 119.009f;
+		String stringFirst = "Apple", stringSecond = "Peach", stringThird = "Banana";
 		
 		maximumInteger(intFirst, intSecond, intThird);
 		System.out.println("The Maximum Integer Number is  :: "+ maxInt);
 	
 		maximumFloat(floatFirst, floatSecond, floatThird);
 		System.out.println("The Maximum Float Number is  :: "+ maxFloat);
+		
+		maximumString(stringFirst, stringSecond, stringThird);
+		System.out.println("The Maximum String Number is  :: "+ maxString);
+		
 	}
 
 }
