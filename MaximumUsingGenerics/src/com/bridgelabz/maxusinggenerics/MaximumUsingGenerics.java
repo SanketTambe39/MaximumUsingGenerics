39efc360af1cac4feef3e64480e35e18df04041c
SanketTambe39 <sankettambe39@gmail.com>
2021-07-01 21:07:29 +0530

    ADDED: Options for CompareTo and Sorting method

--- MaximumUsingGenerics/src/com/bridgelabz/maxusinggenerics/MaximumUsingGenerics.java
+++ MaximumUsingGenerics/src/com/bridgelabz/maxusinggenerics/MaximumUsingGenerics.java
@@ -1,44 +1,80 @@
 package com.bridgelabz.maxusinggenerics;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
+import org.omg.CORBA.portable.ValueOutputStream;
+
 public class MaximumUsingGenerics<T extends Comparable<T>> {
 	
 	private T firstVariable;
 	private T secondVariable;
 	private T thirdVariable;
+	private T[] arrayOfVariables;
 		
-	public MaximumUsingGenerics(T firstVariable, T secondVariable, T thirdVariable) {
+	
+
+	public MaximumUsingGenerics(T firstVariable, T secondVariable, T thirdVariable, T[] arrayOfVariables) {
 		super();
 		this.firstVariable = firstVariable;
 		this.secondVariable = secondVariable;
 		this.thirdVariable = thirdVariable;
+		this.arrayOfVariables = arrayOfVariables;
 	}
 
 	public void findMax() 
 	{
-		maximum(this.firstVariable, this.secondVariable, this.thirdVariable);
+		maximum(this.firstVariable, this.secondVariable, this.thirdVariable,this.arrayOfVariables);
 	}
 	
-	public static <E extends Comparable<E>> E maximum(E firstVariable,E secondVariable,E thirdVariable) {
-		E max = firstVariable;
-		if(secondVariable.compareTo(max) > 0)
-		{
-			max = secondVariable;
+	public static <E extends Comparable<E>> void maximum(E firstVariable,E secondVariable,E thirdVariable,E[] arrayOfVariables) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("\t\tChoose From the following by which you want to check ::\n \t\t 1) Get Maximum by compareTo method \n \t\t 2) Get Maximum "
+				+ "by Sorting method");
+		int option = scanner.nextInt();
+//		scanner.close();
+		switch (option) {
+		case 1:
+			E max = firstVariable;
+			if(secondVariable.compareTo(max) > 0)
+			{
+				max = secondVariable;
+			}
+			if(thirdVariable.compareTo(max) > 0)
+			{
+				max = thirdVariable;
+			}
+			System.out.println("\t\tThe Maximum using CompareTo method is ::"+ max+"\n");			
+			break;
+		case 2:
+			Arrays.sort(arrayOfVariables);
+			System.out.println("\t\tThe Maximum using Sort method is ::" + arrayOfVariables[2]+"\n");
+			break;
+
+		default:
+			System.out.println("Wrong !! Input is wrong.....!!");
+			break;
 		}
-		if(thirdVariable.compareTo(max) > 0)
-		{
-			max = thirdVariable;
-		}
-		return max;
+		
 	}
 		
 	public static void main(String[] args) {
 		
 		Integer intFirst = 23, intSecond = 52, intThird = 92;
-		Float floatFirst = 121.03f, floatSecond = 235.736f, floatThird = 119.009f;
-		String stringFirst = "Apple", stringSecond = "Peach", stringThird = "Banana";
+		Integer[] arrayOfInteger = {intFirst,intSecond,intThird};
 		
-		System.out.println("The Maximum Integer Number is  :: "+ maximum(intFirst,intSecond,intThird));	
-		System.out.println("The Maximum Float Number is  :: "+ maximum(floatFirst, floatSecond, floatThird));
-		System.out.println("The Maximum String Number is  :: "+maximum(stringFirst, stringSecond,stringThird));
+		Float floatFirst = 121.03f, floatSecond = 235.736f, floatThird = 119.009f;
+		Float[] arrayOfFloat = {floatFirst,floatSecond,floatThird};
+		
+		String stringFirst = "Apple", stringSecond = "Peach", stringThird = "Banana";
+		String[] arrayOfString = {stringFirst,stringSecond,stringThird};
+		
+		MaximumUsingGenerics <Integer> integerValue = new MaximumUsingGenerics<Integer>(intFirst,intSecond,intThird,arrayOfInteger);
+		integerValue.findMax();
+		MaximumUsingGenerics <Float> floatValue = new MaximumUsingGenerics<Float>(floatFirst, floatSecond, floatThird,arrayOfFloat);
+		floatValue.findMax();
+		MaximumUsingGenerics <String> stringValue = new MaximumUsingGenerics<String>(stringFirst, stringSecond, stringThird,arrayOfString);
+		stringValue.findMax();
 	}
 }
